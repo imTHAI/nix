c1fb51546794aad7e391b683a40ed36f586b1f49
imTHAI <36070606+imTHAI@users.noreply.github.com>
2026-06-04 08:05:58 +0200

    🔧 chore(shell): use rsync -avh --progress for visible per-file sync feedback

--- home/kamino/shell.nix
+++ home/kamino/shell.nix
@@ -20,9 +20,9 @@
       dl         = "aria2c -x4 --dir=$HOME/Downloads";
       ogg2m4a    = "audio_convert ogg";
       flac2m4a   = "audio_convert flac";
-      sync_photoslib = "rsync -ah --info=progress2 --exclude='.DS_Store' --delete $HOME/Pictures/Photos\\ Library.photoslibrary coruscant:/mnt/user/backups/";
-      sync_calibre   = "rsync -ah --info=progress2 --exclude='.DS_Store' --delete /Volumes/TB_500Go/Librairie\\ Calibre coruscant:/mnt/user/media/books/";
-      sync_bin       = "rsync -ah --info=progress2 --exclude='.DS_Store' --delete $HOME/Applications/bin coruscant:/mnt/user/backups/";
+      sync_photoslib = "rsync -avh --progress --exclude='.DS_Store' --delete $HOME/Pictures/Photos\\ Library.photoslibrary coruscant:/mnt/user/backups/";
+      sync_calibre   = "rsync -avh --progress --exclude='.DS_Store' --delete /Volumes/TB_500Go/Librairie\\ Calibre coruscant:/mnt/user/media/books/";
+      sync_bin       = "rsync -avh --progress --exclude='.DS_Store' --delete $HOME/Applications/bin coruscant:/mnt/user/backups/";
       sync_homedir   = "rsync -vah -e ssh --exclude=\".git\" --exclude=\".venv\" --exclude=\".DS_Store\" --exclude=\"Survivalisme\" --exclude=\"Family_Media_Library\" --delete coruscant:/mnt/user/homedir-pbear/ \"$HOME/Library/Mobile Documents/com~apple~CloudDocs/Backups/homedir-pbear/\"";
     };
     initContent = ''
