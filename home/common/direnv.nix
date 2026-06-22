a12263c2989a3adbc5227031dd0edb33e0f008c6
imTHAI <36070606+imTHAI@users.noreply.github.com>
2026-06-22 19:25:37 +0200

    🔧 chore: suppress direnv verbose export log

--- home/common/direnv.nix
+++ home/common/direnv.nix
@@ -2,5 +2,9 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    # suppress the verbose "export +AR +AS..." lines on shell load
+    stdlib = ''
+      export DIRENV_LOG_FORMAT=""
+    '';
   };
 }
