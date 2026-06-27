31b8e9178f75da53ae78fa5b1916c479a1db410c
imTHAI <36070606+imTHAI@users.noreply.github.com>
2026-06-27 10:49:28 +0200

    🔧 chore(darwin): set sudo Touch ID timeout to 30 minutes

--- system/darwin.nix
+++ system/darwin.nix
@@ -11,6 +11,7 @@
   nix.optimise.automatic = true;
 
   security.pam.services.sudo_local.touchIdAuth = true;
+  security.sudo.extraConfig = "Defaults timestamp_timeout=30";
 
   system.defaults = {
     dock = {
