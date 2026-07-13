4e2c7f2a50449b9f3cd7b89b1a27060072d4dc80
imTHAI <36070606+imTHAI@users.noreply.github.com>
2026-07-13 19:58:03 +0200

    🔒 feat(git): sign commits/tags with SSH key (Vigilant Mode protection)

--- home/common/git.nix
+++ home/common/git.nix
@@ -7,7 +7,11 @@
       user = {
         name  = "imTHAI";
         email = "36070606+imTHAI@users.noreply.github.com";
+        signingkey = "~/.ssh/id_ed25519.pub";
       };
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      tag.gpgsign = true;
       push.autoSetupRemote = true;  # plus jamais "no upstream branch"
     };
   };
